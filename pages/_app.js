086903f3cf6031aa39874e5e2c3b2feba1315bf6
Monisha <monisha.phi@gmail.com>
2023-01-25 06:18:13 +0530

    Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import {Layout} from '../components';
-import '../styles/globals.css'
+import { Layout } from '../components';
+
+
+
+import '../styles/globals.css';
+import { StateContext } from '../context/StateContext';
 
 function MyApp({ Component, pageProps }) {
-  return ( 
-  
-  <Layout>
-    
-  <Component {...pageProps}  /> 
-    
-  </Layout>
-    
+
+
+   
+
+    return (
+        <StateContext>
+            <Layout>
+                
+                <Component {...pageProps} />
+
+            </Layout>
+
+        </StateContext>
     )
 }
 
